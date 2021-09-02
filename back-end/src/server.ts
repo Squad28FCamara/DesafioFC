4545f9d472cc00f206c169af8344faf169561000
Matheus Freitas <matheusalvesdefreitas@gmail.com>
2021-09-02 19:18:21 -0300

    CreateUserService created, AppError file working

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes';
+import routes from './users/routes';
 
 import './database';
 
---
+++ back-end/src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import AppError from 'errors/AppError';
+import User from 'users/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+//serviço para criar usuario
+class CreateUserService {
+  async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    //verifica se o email ja está no banco de dados
+    if (checkUserExists) {
+      throw new AppError('Email address already used');
+    }
+
+    //criptografa a senha
+    const hashedPassword = await hash(password, 8);
+
+    //cria o usuario
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //salva o usuario
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
