1eb27facb7a17db7b19f92c644155f400f271514
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-09-08 09:08:13 -0300

    winston and .log removed

--- front-end/router.js
+++ front-end/router.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.render("dashboard"));
 
+router.get("/covid", (req, res) => res.render("covid"));
+
 export default router;
--- front-end/server.js
+++ front-end/server.js
@@ -1,24 +1,8 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import router from "./router.js";
-import winston from "winston";
-import path from "path";
 
 const app = express();
-
-const { combine, timestamp, label, printf } = winston.format;
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp}, [${label}] ${level}: message`;
-});
-
-global.logger = winston.createLogger({
-  level: "silly",
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "fcamara.log" }),
-  ],
-  format: combine(label({ label: "fcamara" }), timestamp(), myFormat),
-});
 
 app.use(express.static("public"));
 app.use(express.static("assets"));
@@ -29,15 +13,8 @@ app.set("view engine", "njk");
 nunjucks.configure("views", {
   express: app,
   noCache: true,
-  autoescape: false,
 });
 
 app.get("/", router);
 
-app.listen(5000, () => {
-  try {
-    global.logger.info("Server is running");
-  } catch (err) {
-    global.logger.error(err);
-  }
-});
+app.listen(5000, () => console.log("Server is running"));
