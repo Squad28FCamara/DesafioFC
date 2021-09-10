147fcb44e7960e9458eaa28968056d8005da0225
Matheus Freitas <matheusalvesdefreitas@gmail.com>
2021-09-10 18:08:33 -0300

    layout changed

--- front-end/router.js
+++ front-end/router.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const { readFileSync } = require('fs');
 
+const listUserAppointments = require('./services/listUserAppointment');
 const create = require('./user.reservation.js');
 
 const router = express.Router();
 
 const data = JSON.parse(readFileSync('data.json').toString());
 
-router.get('/', (req, res) => res.render('dashboard', { data }));
+router.get('/', async (req, res) => {
+  const reservations = await listUserAppointments(
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzEzMDQzNTksImV4cCI6MTYzMTM5MDc1OSwic3ViIjoiN2I4MjEwMWYtNzllZS00MGMwLTk2OGUtNGRmYTA5NWUyNTY0In0.ZwitHJRSQj-X0jWsreu3XEula_J5HBj5dM8escvKDGg'
+  );
+
+  return res.render('dashboard', { data: { reservations } });
+});
 
 router.get('/reserve', (req, res) => res.render('reserve', { data }));
 
