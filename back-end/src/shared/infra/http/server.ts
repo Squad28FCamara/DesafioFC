a7f77b9219d2b2cddb13c76a3a0be94078a213b5
Matheus Freitas <matheusalvesdefreitas@gmail.com>
2021-09-10 14:21:20 -0300

    test routes removed

--- back-end/src/shared/infra/http/server.ts
+++ back-end/src/shared/infra/http/server.ts
@@ -12,12 +12,6 @@ app.use(express.json());
 app.use(routes);
 app.use(error);
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello World!',
-  });
-});
-
 app.listen(3333, () => {
   console.log('Server Started');
   console.log('link: http://localhost:3333');
