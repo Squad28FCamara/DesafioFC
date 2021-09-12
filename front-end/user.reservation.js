3a6a0f57ad43af720a2b918488cdab4c63a6ab79
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-09-12 01:22:29 -0300

    refactored all code, layout and styling files

--- front-end/user.reservation.js
+++ front-end/user.reservation.js
@@ -7,9 +7,11 @@ const data = JSON.parse(await readFile("data.json"));
 export function create(req, res) {
   const reservation = req.body;
 
-  data.reservations.push(reservation);
-
-  writeFile("data.json", JSON.stringify(data, null, 2));
-
-  return res.send(data);
+  if (reservation.pole == "" || reservation.date == "") {
+    console.log("Nop");
+  } else {
+    data.reservations.push(reservation);
+    writeFile("data.json", JSON.stringify(data, null, 2));
+    return res.send(data);
+  }
 }
