03497edbeb12b42a73d8b039dc60ee882d650b93
Matheus Freitas <matheusalvesdefreitas@gmail.com>
2021-09-14 14:39:18 -0300

    reserve script working

--- front-end/user.reservation.js
+++ front-end/user.reservation.js
@@ -5,11 +5,17 @@ const data = JSON.parse(readFileSync('data.json').toString());
 function create(req, res) {
   const reservation = req.body;
 
-  if (reservation.pole == '' || reservation.date == '') {
+  if (
+    !reservation.pole ||
+    !reservation.seat ||
+    !reservation.station ||
+    !reservation.date ||
+    !reservation.floor
+  ) {
     console.log('Nop');
   } else {
     data.reservations.push(reservation);
-    writeFile('data.json', JSON.stringify(data, null, 2));
+    writeFileSync('data.json', JSON.stringify(data, null, 2));
     return res.send(data);
   }
 }
